e54cdb307f9157c2136ccc842bd9bfd7e06e62b3
gmaxym <gorbenkomaxym@gmail.com>
2020-10-12 04:26:46 +0300

    Updates

---
+++ precache-manifest.3d103f602a6635ded324139970c8cb14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99144ba5b700879f3121",
+    "url": "/myBlogCaspio/static/css/main.1b5848c3.chunk.css"
+  },
+  {
+    "revision": "99144ba5b700879f3121",
+    "url": "/myBlogCaspio/static/js/main.5e08f254.chunk.js"
+  },
+  {
+    "revision": "eef5cb6cf113b5d0e27f",
+    "url": "/myBlogCaspio/static/js/runtime~main.8b404fd1.js"
+  },
+  {
+    "revision": "1bf0a4a690c51e6ac32d",
+    "url": "/myBlogCaspio/static/js/2.97c7e762.chunk.js"
+  },
+  {
+    "revision": "56cbee07d4e3ab5f3b7b8d7b998fb7f6",
+    "url": "/myBlogCaspio/index.html"
+  }
+];
